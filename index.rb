c0467f769ffb1fcb8ad966ef3f37e32bb42db619
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 16:07:54 -0700

    creates and lists a post on a thread page

--- index.rb
+++ index.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 
@@ -6,13 +7,19 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/index.db")
 class Comment
   include DataMapper::Resource
   property :id, Serial
+  property :title, Text
   property :content, Text, :required => true
-  property :complete, Boolean, :required => true, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
 
 DataMapper.finalize.auto_upgrade!
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 
 get '/' do
   @comments = Comment.all :order => :id.desc
@@ -23,6 +30,7 @@ end
 post '/' do
   comment = Comment.new
   comment.content = params[:content]
+  comment.title = params[:title]
   comment.created_at = Time.now
   comment.updated_at = Time.now
   comment.save
