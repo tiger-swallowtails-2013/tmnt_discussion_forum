461d07596143995436c7de0533d3b9c3d0ad2e7a
dbcapprentice <dbcapprentice@gmail.com>
2013-09-17 16:32:57 -0700

    sets views for sinatra

--- config/main.rb
+++ config/main.rb
@@ -1,12 +1,15 @@
 require 'pathname'
 require 'sqlite3'
 require 'active_record'
+require 'sinatra'
 
 APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 
 APP_NAME = APP_ROOT.basename.to_s
 
 DB_PATH  = APP_ROOT.join('db', APP_NAME + ".db").to_s
+
+VIEW_PATH = APP_ROOT.join('app','views').to_s
 
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
@@ -15,3 +18,15 @@ end
 
 ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
                                         :database => DB_PATH
+
+
+
+
+
+p VIEW_PATH
+
+
+
+configure do
+  set :views, VIEW_PATH
+end
