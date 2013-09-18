6e6138c7532aa82cbc2f0e097aa2b908ca957545
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 18:10:07 -0700

    merges the ar structure with the basic front end construction

--- config/main.rb
+++ config/main.rb
@@ -16,6 +16,8 @@ MIGRATION_PATH = APP_ROOT.join('db','migrate',new_migration_filename).to_s
 
 VIEW_PATH = APP_ROOT.join('app','views').to_s
 
+PUBLIC_PATH = APP_ROOT.join('public').to_s
+
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
   autoload ActiveSupport::Inflector.camelize(filename), model_file
@@ -26,4 +28,8 @@ ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
 
 configure do
   set :views, VIEW_PATH
+end
+
+configure do 
+  set :public_folder, PUBLIC_PATH
 end
